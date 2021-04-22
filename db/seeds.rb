befa7bb1ad2adb2a6790091d57a1e5ef99e16a9d
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-04-22 20:46:34 +0100

    added faker gem file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create (
+  title: Faker::DcComics.title,
+  content: Faker::DcComics.hero,
+  rating: rand(1..5)
+   )
+
+end
