e61e5426fd73a370028184158afc7a23c4c5d04a
Jonas Noori <jonasgeneralassembly@gmail.com>
2021-04-23 14:45:24 +0100

    updated seed file with printed message after db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 10.times do
-  Article.create (
+  Article.create(
   title: Faker::DcComics.title,
   content: Faker::DcComics.hero,
    )
-
+  puts "article #{Article} is created"
 end
+puts "done"
